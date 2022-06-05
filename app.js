4a725b7a9d72a19df10cd450321e97bdbde14bc3
EliasLahham <eliaslahham@yahoo.com>
2022-06-05 19:47:44 -0400

    Changed content_script + fixed page load bug

--- app.js
+++ app.js
@@ -1,27 +1,39 @@
 const app = document.querySelector('ytd-app');
 
-function scrapeForVideos() {
-	app.querySelectorAll('ytd-grid-video-renderer').forEach(hideVideo);
+function startUp() {
+	if (!location.pathname.endsWith('/feed/subscriptions')) {
+		return;
+	}
+	if (app) {
+		console.log('%c[Hide-Subscription-Videos] is running!', 'color: #03fc24;');
+		scrapeForVideos();
+	}
 }
 
-function hideVideo(video, index) {
-	browser.storage.local.get().then(function(result) {
-		var keywords = Object.keys(result);
-		var title = video.innerText.toLowerCase();
-		keywords.every(function(keyword) {
-			if (title.includes(keyword)) {
-				console.log(`Hiding video: ${video.innerText}`);
-				video.remove();
-				return false;
-			}
-			return true;
+function scrapeForVideos() {
+	browser.storage.local.get().then(keywords => {
+		if (Object.keys(keywords).length === 0) {
+			return;
+		}
+		app.querySelectorAll('ytd-grid-video-renderer').forEach(function (video, index) {
+			hideVideo(keywords, video, index)
 		});
+	}).catch(error => {
+		console.log('[Hide-Subscription-Videos] Error: ' + error);
 	});
 }
 
-if (app) {
-	console.log('%c[Hide-Subscription-Videos] is running!', 'color: #03fc24;');
-	app.addEventListener('yt-navigate-finish', scrapeForVideos);
+function hideVideo(keywords, video, index) {
+	var keywords = Object.keys(keywords);
+	var title = video.innerText.toLowerCase();
+	keywords.every(function(keyword) {
+		if (title.includes(keyword)) {
+			console.log(`Hiding video: ${video.innerText}`);
+			video.remove();
+			return false;
+		}
+		return true;
+	});
 }
 
-scrapeForVideos();
+document.addEventListener('yt-navigate-finish', startUp);
